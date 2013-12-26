c2b3dfadb9955522f3c105504b5f14cfed5bf0f8
Roger Almeida <roger.eduardo@gmail.com>
2013-12-26 16:51:37 -0200

    Added default vagrant+puppet recipe

---
+++ Vagrantfile
@@ -0,0 +1,29 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  # Every Vagrant virtual environment requires a box to build off of.
+  #config.vm.box = "precise64"
+
+  # The url from where the 'config.vm.box' box will be fetched if it
+  # doesn't already exist on the user's system.
+  #config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.box = "Ubuntu 12.10 Quantal x86_64"
+  config.vm.box_url = "https://github.com/downloads/roderik/VagrantQuantal64Box/quantal64.box"
+
+  # Forward a port from the guest to the host, which allows for outside
+  # computers to access the VM, whereas host only networking does not.
+  #config.vm.forward_port 80, 8080
+  config.vm.network :forwarded_port, guest: 8888, host: 3000
+  #config.vm.forward_port 8000, 8001
+  #config.vm.forward_port 9999, 9998
+
+  config.vm.network :public_network, :bridge => 'en1: Wi-Fi (AirPort)'
+
+  config.vm.provision "puppet" do |puppet|
+    puppet.manifests_path = "manifests"
+    puppet.manifest_file = "default.pp"
+    puppet.options = "--verbose"
+  end
+
+end
---
+++ manifests/default.pp
@@ -0,0 +1,49 @@
+exec { "apt-get update":
+    command => "/usr/bin/apt-get update",
+    onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
+}
+
+package { "ipython-notebook":
+    ensure => "installed",
+    require  => Exec['apt-get update'],
+}
+
+package { "python-dev":
+    ensure => "installed",
+    require  => Exec['apt-get update'],
+}
+
+package { "sqlite3":
+    ensure => "installed",
+    require => Exec["apt-get update"],
+}
+
+package { "libsqlite3-dev":
+    ensure => "installed",
+    require => Exec["apt-get update"],
+}
+package { "xclip":
+    ensure => "installed",
+    require  => Exec['apt-get update'],
+}
+
+package { "git-core":
+	ensure => "installed",
+}
+
+package {'wget':
+	ensure => 'installed',
+	require => Package['python-dev'],
+}
+
+exec { "install easy_install":
+    command => "wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python",
+    path => "/usr/bin/",
+    creates => "/usr/bin/easy_install",
+    require  => Package["python-dev"],
+}
+
+exec { "install beautifulsoup4":
+    command => "/usr/local/bin/easy_install beautifulsoup4",
+    require => Exec["install easy_install"],
+}
