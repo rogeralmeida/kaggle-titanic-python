792a140caf8ea20d173fa9c623be5cc970636f0d
Roger Almeida <roger.eduardo@gmail.com>
2013-12-29 12:19:56 -0200

    Organizing dependencies in puppet recipe

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,38 +2,12 @@ exec { "apt-get update":
     command => "/usr/bin/apt-get update",
     onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
 }
+$dependecies=["ipython-notebook", "python-numpy", "python-scipy", "python-matplotlib", "xclip", "wget",
+"python-pandas", "python-sympy", "python-nose", "python-dev", "sqlite3", "libsqlite3-dev", "git-core", "python-mlpy"]
 
-package { "ipython-notebook":
-    ensure => "installed",
-    require  => Exec['apt-get update'],
-}
-
-package { "python-dev":
-    ensure => "installed",
-    require  => Exec['apt-get update'],
-}
-
-package { "sqlite3":
-    ensure => "installed",
-    require => Exec["apt-get update"],
-}
-
-package { "libsqlite3-dev":
-    ensure => "installed",
-    require => Exec["apt-get update"],
-}
-package { "xclip":
-    ensure => "installed",
-    require  => Exec['apt-get update'],
-}
-
-package { "git-core":
-	ensure => "installed",
-}
-
-package {'wget':
+package {$dependecies:
 	ensure => 'installed',
-	require => Package['python-dev'],
+    require  => Exec['apt-get update'],
 }
 
 exec { "install easy_install":
